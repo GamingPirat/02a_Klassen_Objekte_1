37ffbebb1db26e2700466266ff10c868049ceb15
Michael <GamingPirat@gmail.com>
2024-07-12 12:50:54 +0200

    02 / 01 Erzeugen eines neuen Objekts

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,13 @@ public class App {
     
     public static void main(String[] args) {
 
+        //Instanziierung --> Erzeugen eines neuen Objekts
+        // System.out.println(new Cat());
 
+        //cat --> Referenzvariable
+        // --> Referenztyp(Klasse)
+      Cat cat = new Cat(); 
+      System.out.println(cat); 
     }
 
    
